7e10609cf428ce25ff0072217d75376832726f60
NJL7502L <g.hana1211.tako@gmail.com>
2020-07-11 00:36:21 +0900

    pwm周波数とLPF定数を決めた。

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,11 @@
 
 void setup() {
   // put your setup code here, to run once:
+  analogWriteFrequency(10,234375);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  analogWrite(10, 256*((millis() % 100)/100.0f));
+  
 }
