73e8a39b0ba998e15fa87540c3441d468cc458f9
NJL7502L <g.hana1211.tako@gmail.com>
2020-07-11 15:29:12 +0900

    配列の最大値,最小値から自動でmapするようにした。

--- src/main.cpp
+++ src/main.cpp
@@ -3,40 +3,64 @@
 #include "CartesianCoordinates.h"
 #include "Plotter.h"
 
-int x;
-int y;
+const int T_MIN = 0.0;
+const int T_MAX = 200.0;
+const int ARR_LENGTH = 10000;
+
+const float T_RES = ((T_MAX - T_MIN)/(float)ARR_LENGTH);
+
+int a;
+int b;
 Plotter p;
 
 class CarCoor{
 public:
+  float x;
+  float y;
+
   int x_i;
   int y_i;
 };
 
-CarCoor figure[10000];
-// class Minmax{
-// public:
-//   float min;
-//   float max;
-// };
-// Minmax m_x,m_y;
+float x_min;
+float x_max;
+float y_min;
+float y_max;
 
+CarCoor figure[ARR_LENGTH];
 
 double mapf(double val, double in_min, double in_max, double out_min, double out_max) {
   return (val - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void store_parameter(){
-  for (int i = 0; i < 10000; i+=1){
-    figure[i].x_i = mapf(return_x(200*(i/10000.0)),1480.15,1662.39,0,255);
-    figure[i].y_i = mapf(return_y(200*(i/10000.0)),-1003.87,-821.45,0,255);
+  for (int i = 0; i < ARR_LENGTH; i++){
+    figure[i].x = return_x(i*T_RES);
+    figure[i].y = return_y(i*T_RES);
+
+    if(i == 0){
+      x_min = figure[i].x;
+      x_max = figure[i].x;
+      y_min = figure[i].y;
+      y_max = figure[i].y;
+    }else{
+      x_min = min(x_min,figure[i].x);
+      x_max = max(x_max,figure[i].x);
+      y_min = min(y_min,figure[i].y);
+      y_max = max(y_max,figure[i].y);
+    }
+
+  }
+  for (int i = 0; i < ARR_LENGTH; i++){
+    figure[i].x_i = mapf(figure[i].x,x_min,x_max,0,255);
+    figure[i].y_i = mapf(figure[i].y,y_min,y_max,0,255);
   }
 }
 
 void setup() {
   p.Begin();
   // put your setup code here, to run once:
-  p.AddXYGraph( "X-Y graph w/ 500 points", 500, "x axis", x, "y axis", y );
+  p.AddXYGraph( "X-Y graph w/ 500 points", 500, "x axis", a, "y axis", b );
 
   analogWriteFrequency(2,234375);
   analogWriteFrequency(3,234375);
@@ -48,55 +72,21 @@ void setup() {
   // m_y.min = return_y(0.1);
   // m_y.max = return_y(0.1);
   store_parameter();
+  digitalWrite(LED_BUILTIN,HIGH);
 }
 
-// void print_minmax(){
-//   // put your main code here, to run repeatedly:
-//   // analogWrite(10, 256*((millis() % 100)/100.0f));
-//   static float t = 0;
-
-//   if(t<200){
-//     t+=0.1;
-//   }else{
-//     t = 0.1;
-//   }
-//   x = return_x(t);
-//   y = return_y(t);
-
-//   m_x.min = min(m_x.min ,x);
-//   m_x.max = max(m_x.max ,x);
-
-//   m_y.min = min(m_y.min ,y);
-//   m_y.max = max(m_y.max ,y);
-
-//   Serial.print("\tx min:\t");
-//   Serial.print(m_x.min);
-//   Serial.print("\tx max:\t");
-//   Serial.print(m_x.max);
-//   Serial.print("\ty min:\t");
-//   Serial.print(m_y.min);
-//   Serial.print("\ty max:\t");
-//   Serial.print(m_y.max);
-//   Serial.print("\tt :\t");
-//   Serial.print(t);
-//   Serial.println();
-//   // p.Plot();
-// }
-
 void loop() {
-  // put your main code here, to run repeatedly:
-  // analogWrite(10, 256*((millis() % 100)/100.0f));
   static int t = 0;
-  if(t<10000){
+  if(t<ARR_LENGTH){
     t++;
   }else{
     t = 0;
   }
 
-  x = figure[t].x_i;
-  y = figure[t].y_i;
+  a = figure[t].x_i;
+  b = figure[t].y_i;
   // p.Plot();
   delayMicroseconds(100);
-  analogWrite(2, x);
-  analogWrite(3, y);
+  analogWrite(2, a);
+  analogWrite(3, b);
 }
